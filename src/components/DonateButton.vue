5b246f63d8ea0602773ae06b255e154ea22c66b0
angjamais <ajamais3105@gmail.com>
2020-10-17 23:09:50 +0800

    -added static donate page -to do: routes for each photo, route for donate button

---
+++ src/components/DonateButton.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+    <button>Donate Now</button>
+</div>
+</template>
+
+<script></script>
+
+<style scope>
+button{
+    font-family:'Arial Narrow Bold', sans-serif;
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    font-weight: bold;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,11 @@
+import Donate from './components/Donate.vue'
 import Dummy from './components/HelloWorld.vue'
 
 export default[
     { path: '/', component: Dummy},
     { path: '/join_us', component: Dummy},
     { path: '/requests', component: Dummy},
-    { path: '/donate', component: Dummy },
+    { path: '/donate', component: Donate },
     { path: '/contact_us', component: Dummy },
     { path: '/inbox', component: Dummy },
     { path: '/personal', component: Dummy }
