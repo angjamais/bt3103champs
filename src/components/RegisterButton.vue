02604316ea6c552bf2d8bf25c1dcbc8745aa47be
bctangbc <bcbc12138@gmail.com>
2020-10-18 17:14:38 +0800

    Added home page, join us page, register pages for households and volunteers, custom buttons that link the added pages, added in new images in asset folder, updated styling in header and footer.

---
+++ src/components/RegisterButton.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <button>Register ></button>
+</div>
+</template>
+
+<script>
+</script>
+
+<style scope>
+button{
+    background-color: rgb(102, 240, 187);
+    border: 0px;
+    width: 190px;
+    height: 60px;
+    text-align:'center';
+    font-size:25px;
+    color:black;
+    padding:4px;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import Dummy from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
+import JoinUs from './components/JoinUs.vue'
+import RegHousehold from './components/RegisterHousehold.vue'
+import RegVolunteer from './components/RegisterVolunteer.vue'
 
 export default[
-    { path: '/', component: Dummy},
-    { path: '/join_us', component: Dummy},
+    { path: '/', component: Home},
+    { path: '/join_us', component: JoinUs},
     { path: '/requests', component: Dummy},
+    { path: '/registerhousehold', component: RegHousehold},
+    { path: '/registervolunteer', component: RegVolunteer},
     { path: '/donate', component: Dummy },
     { path: '/contact_us', component: Dummy },
     { path: '/inbox', component: Dummy },
