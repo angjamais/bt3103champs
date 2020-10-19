28cefdfa18f8a61a44766869dd33cb08fc0d7261
linhuaian <65528303+linhuaian@users.noreply.github.com>
2020-10-19 23:40:51 +0800

    Added requests page, imported vue boostrap?

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import Dummy from './components/HelloWorld.vue'
+import Requests from './components/Requests.vue'
 
 export default[
     { path: '/', component: Dummy},
     { path: '/join_us', component: Dummy},
-    { path: '/requests', component: Dummy},
+    { path: '/requests', component: Requests},
     { path: '/donate', component: Dummy },
     { path: '/contact_us', component: Dummy },
     { path: '/inbox', component: Dummy },
